2b6000a0f371a73a910e85cffa769a3a59a09c93
harsh1dev <harshkumar1.dev@gmail.com>
2024-10-23 13:31:30 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
 
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 function generateAlphaId() {
@@ -16,14 +18,28 @@ function generateAlphaId() {
         .toLowerCase();
 }
 
+function formatJSON(jsonString) {
+    try {
+        const parsed = JSON.parse(jsonString);
+        const formatted = JSON.stringify(parsed, null, 2);
+        return formatted;
+    } catch (error) {
+        return null;
+    }
+}
 
 const db = {};
 
+
 app.post('/data', (req, res) => {
     const { data } = req.body;
+    // const formatedData = formatJSON(data);
+    // if (formatedData === null) {
+    //     return res.status(400).json({ error: 'Invalid JSON data' });
+    // }
     const id = generateAlphaId();
     db[id] = data;
-    res.json({ "url": id });
+    return res.json({ "url": id });
 });
 
 app.get('/db/:id', (req, res) => {
